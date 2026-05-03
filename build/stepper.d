e2b2f6b54f9ccb77cfa33550ffed0e4be333a2d6
bg4dzy <bg4dzy@users.noreply.github.com>
2026-05-03 22:52:31 +0800

    Add files via upload

---
+++ build/stepper.d
@@ -0,0 +1,50 @@
+build/stepper.o: grbl/stepper.c grbl/grbl.h grbl/config.h \
+ grbl/nuts_bolts.h grbl/settings.h grbl/system.h grbl/defaults.h \
+ grbl/cpu_map.h grbl/planner.h grbl/backlash.h grbl/coolant_control.h \
+ grbl/eeprom.h grbl/gcode.h grbl/limits.h grbl/motion_control.h \
+ grbl/print.h grbl/probe.h grbl/protocol.h grbl/report.h grbl/serial.h \
+ grbl/spindle_control.h grbl/stepper.h grbl/jog.h
+
+grbl/grbl.h:
+
+grbl/config.h:
+
+grbl/nuts_bolts.h:
+
+grbl/settings.h:
+
+grbl/system.h:
+
+grbl/defaults.h:
+
+grbl/cpu_map.h:
+
+grbl/planner.h:
+
+grbl/backlash.h:
+
+grbl/coolant_control.h:
+
+grbl/eeprom.h:
+
+grbl/gcode.h:
+
+grbl/limits.h:
+
+grbl/motion_control.h:
+
+grbl/print.h:
+
+grbl/probe.h:
+
+grbl/protocol.h:
+
+grbl/report.h:
+
+grbl/serial.h:
+
+grbl/spindle_control.h:
+
+grbl/stepper.h:
+
+grbl/jog.h:
